fa889c7b524a4baa3f477918fb75e291a8521bae
Amy Zhang <yun@Yuns-MacBook-Pro.local>
2018-10-06 13:22:54 -0500

    add data service

---
+++ oj-client/src/app/problem-list/problem-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Problem } from '../../model/problem.model';
+import { DataService } from '../services/data.service';
+
+
+@Component({
+  selector: 'app-problem-list',
+  templateUrl: './problem-list.component.html',
+  styleUrls: ['./problem-list.component.css']
+})
+export class ProblemListComponent implements OnInit {
+  
+  problems: Problem[];
+  problem: Problem;
+
+  constructor(private dataService: DataService) { }
+
+  // initialize problems in this class
+  ngOnInit() {
+    this.getProblems();
+  }
+
+  getProblems() {
+  	this.problems = this.dataService.getProblems();
+  }
+
+  getProblem(id: number) {
+  	this.problem = this.dataService.getProblem(id);
+  }
+
+}
---
+++ oj-client/src/app/services/data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Problem } from '../../model/problem.model';
+import { PROBLEMS } from '../mock-problems';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  // list of problems
+  problems: Problem[] = PROBLEMS;
+
+  constructor() { }
+
+  // return a list of problems
+  getProblems(): Problem[] {
+  	return this.problems;
+  }
+
+  // input: id
+  // return a problem by id
+  getProblem(id: number): Problem {
+    //return this.problem.find((problem) => problem.id ===id);
+  	for(let problem of this.problems){
+  	  if(problem.id === id){
+  	    return problem;
+  	  }
+  	}
+
+  }
+
+}
